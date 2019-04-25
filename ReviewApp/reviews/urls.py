dd2d612136bf003d3405431d4c73b2c8d3ab03e4
Luke Henry <b5030816@my.shu.ac.uk>
2019-04-25 02:22:14 +0100

    Final

--- ReviewApp/reviews/urls.py
+++ ReviewApp/reviews/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 from . import views
 from users import views as user_views
-from .views import ProductListView, ProductDetailView
+from .views import ProductListView, ProductDetailView 
+#ReviewDetailView, ReviewCreateView, ReviewUpdateView, ReviewDeleteView
 
 urlpatterns = [
 	path('', views.home, name='reviews-home'),
 	path('products/', ProductListView.as_view(), name='reviews-products'),
 	path('products/<int:pk>', ProductDetailView.as_view(), name='product-detail'),
+	# path('reviews/<int:pk>', ReviewDetailView.as_view(), name='review-detail'),
+	# path('reviews/add/', ReviewCreateView.as_view(), name='review-create'),
+	# path('reviews/<int:pk>/update/', ReviewUpdateView.as_view(), name='review-update'),
+	# path('reviews/<int:pk>/delete/', ReviewDeleteView.as_view(), name='review-delete'),
 	path('about/', views.about, name='reviews-about'),
 	path('contact/', views.contact, name='reviews-contact'),
 	path('register/', user_views.register, name='register'),
